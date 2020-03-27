890d9f323d0921faf026d8cb0c8a3cf768046407
mr <info@masujimaryohei.jp>
2020-03-27 11:09:37 +0900

    Add index.html

---
+++ js/ssb.js
@@ -0,0 +1,8 @@
+var langNo = {
+    jp: 1, en: 2, kr: 3, cn: 4
+}
+function onClick() {
+    var no = document.getElementById("no").value;
+    var lang = document.getElementById("lang").value;
+
+}
